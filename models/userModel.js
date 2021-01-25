7b951aef59d89cb159484b10d743c141c54caeff
sahil2343 <sahilpradhan1999@gmail.com>
2021-01-25 10:13:24 +0530

    user signup model

---
+++ models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false,
+  },
+});
+
+const User = mongoose.model("Phonebook", userSchema);
+module.exports = User;
